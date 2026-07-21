adb22149307df27ccdb2bc1a9de8e43a047641e9
Mateus Sena <mateussantos192007@gmail.com>
2026-07-21 18:56:54 -0300

    feat: inicializa projeto de controle de chamados

---
+++ src/test/java/br/com/senai/mateus/controlechamados/ControleChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.mateus.controlechamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
