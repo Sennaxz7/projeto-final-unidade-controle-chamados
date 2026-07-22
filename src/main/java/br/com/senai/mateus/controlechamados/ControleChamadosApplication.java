801f53fc5ea5ecfe25ae136be1016808ebb79bb4
Mateus Sena <mateussantos192007@gmail.com>
2026-07-21 22:14:47 -0300

    feat: inicializa projeto de controle de chamados

---
+++ src/main/java/br/com/senai/mateus/controlechamados/ControleChamadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.mateus.controlechamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleChamadosApplication.class, args);
+	}
+
+}
