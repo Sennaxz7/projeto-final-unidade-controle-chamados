995936b0ef42ab6aeaaf6a695151bae2eab091ec
Mateus Sena <mateussantos192007@gmail.com>
2026-08-22 00:13:59 -0300

    refactor: altera visibilidade do id para private

--- src/main/java/br/com/senai/mateus/controlechamados/entity/Chamado.java
+++ src/main/java/br/com/senai/mateus/controlechamados/entity/Chamado.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Chamado {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
     private String titulo;
     private String descricao;
